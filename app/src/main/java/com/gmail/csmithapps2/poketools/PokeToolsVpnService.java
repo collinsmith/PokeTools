00f4fe0c21bcf71bdff66dbc180536c150f4e868
Collin Smith <collinsmith70@gmail.com>
2016-08-10 22:50:02 -0700

    Deprecated and removed VPN implementation (now going to use API)

--- app/src/main/java/com/gmail/csmithapps2/poketools/PokeToolsVpnService.java
+++ app/src/main/java/com/gmail/csmithapps2/poketools/PokeToolsVpnService.java
@@ -14,6 +14,7 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 
+@Deprecated
 public class PokeToolsVpnService extends VpnService implements Runnable, Handler.Callback {
 
     private static final String TAG = "PokeToolsVpnService";
