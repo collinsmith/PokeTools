6c6f29c7b167c4081eb6aef52543e6b5b6450319
Collin Smith <collinsmith70@gmail.com>
2016-08-10 22:55:19 -0700

    Added LoginActivity

---
+++ app/src/main/java/com/gmail/csmithapps2/poketools/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.csmithapps2.poketools;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
