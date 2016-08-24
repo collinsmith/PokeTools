c33cf8d3cb732148e9f0aa25a999f38577e7fa44
Collin Smith <collinsmith70@gmail.com>
2016-08-24 14:28:13 -0700

    Removed old OAuth2 openid authentication code

--- app/src/main/java/com/poketools/MainActivity.java
+++ app/src/main/java/com/poketools/MainActivity.java
@@ -2,10 +2,6 @@ package com.poketools;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import net.openid.appauth.AuthorizationException;
-import net.openid.appauth.AuthorizationResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        AuthorizationResponse authorizationResponse = AuthorizationResponse.fromIntent(getIntent());
-        AuthorizationException exception = AuthorizationException.fromIntent(getIntent());
-        if (authorizationResponse != null) {
-            // authorization succeeded
-            Log.d(TAG, "authorizationCode=" + authorizationResponse.authorizationCode);
-        } else {
-            // authorization failed, check ex for more details
-            Log.e(TAG, exception.getLocalizedMessage(), exception);
-        }
-
         setContentView(R.layout.main);
     }
 
